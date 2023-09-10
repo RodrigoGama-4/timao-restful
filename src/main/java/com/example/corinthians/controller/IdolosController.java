beb8be5aef501c0d56052cf2d5ccdc2623a5d195
rodrigogama <rodrigogamah4@gmail.com>
2023-09-10 19:45:48 -0300

    delete feito

--- src/main/java/com/example/corinthians/controller/IdolosController.java
+++ src/main/java/com/example/corinthians/controller/IdolosController.java
@@ -4,7 +4,9 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -37,4 +39,12 @@ public class IdolosController {
     .toList();
         return idolosLista;
     }
+
+    @CrossOrigin(origins = "*", allowedHeaders = "*")
+    @DeleteMapping("/{id}")
+    public void deleteIdolo(@PathVariable Long id) {
+        Idolo idolo = repository.findById(id).get();
+        repository.delete(idolo);
+    }
+
 }
