75ec019912941d2d6e4227b69f873f34000c4238
rodrigogama <rodrigogamah4@gmail.com>
2023-11-08 18:19:36 -0300

    segundo endpoint

--- src/main/java/com/example/corinthians/controller/AuthenticationController.java
+++ src/main/java/com/example/corinthians/controller/AuthenticationController.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 import jakarta.validation.Valid;
 
@@ -13,13 +14,19 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.corinthians.domain.user.User;
 import com.example.corinthians.dtos.AuthenticationDTO;
+import com.example.corinthians.dtos.RegisterDTO;
+import com.example.corinthians.repository.UserRepository;
 
 @RestController
 @RequestMapping("auth")
 public class AuthenticationController {
     @Autowired
     private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private UserRepository repository;
 
     @PostMapping("/login")
     public ResponseEntity login(@RequestBody @Valid AuthenticationDTO data ){
@@ -29,6 +36,16 @@ public class AuthenticationController {
         return ResponseEntity.ok().build();
     }
 
-    
+    @PostMapping("/register")
+    public ResponseEntity register(@RequestBody @Valid RegisterDTO data){
+        if(this.repository.findByLogin(data.email()) != null) return ResponseEntity.badRequest().build();
+        
+        String senhaEncript = new BCryptPasswordEncoder().encode(data.senha());
+        User user = new User(data.email(), senhaEncript, data.role());
+
+        this.repository.save(user);
+
+        return ResponseEntity.ok().build();
+    }
 
 }
