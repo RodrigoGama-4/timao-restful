93bc8d41e92515dffcfc4865cb42839b009a4aaa
rodrigogama <rodrigogamah4@gmail.com>
2023-11-09 14:38:25 -0300

    permissao para registrar e logar

--- src/main/java/com/example/corinthians/infra/security/SecurityConfi.java
+++ src/main/java/com/example/corinthians/infra/security/SecurityConfi.java
@@ -22,6 +22,8 @@ public class SecurityConfi {
                 .csrf(csrf -> csrf.disable())
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(authorize -> authorize
+                        .requestMatchers(HttpMethod.POST, "/auth/login").permitAll()
+                        .requestMatchers(HttpMethod.POST, "/auth/register").permitAll()
                         .requestMatchers(HttpMethod.POST, "/idolos").hasRole("ADMIN")
                         .anyRequest().authenticated()
                 )
