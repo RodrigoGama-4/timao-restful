0c0a68a4910c0f7eaac9c4846b5a9b7dcd6c6e98
rodrigogama <rodrigogamah4@gmail.com>
2023-11-12 09:55:33 -0300

    .

--- src/main/java/com/example/corinthians/infra/security/TokenService.java
+++ src/main/java/com/example/corinthians/infra/security/TokenService.java
@@ -4,7 +4,6 @@ import java.time.Instant;
 import java.time.LocalDateTime;
 import java.time.ZoneOffset;
 
-import org.apache.logging.log4j.CloseableThreadContext.Instance;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
