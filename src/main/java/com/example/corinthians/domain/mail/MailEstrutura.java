95b77af357ce81271a898a8737f2079773c4658b
rodrigogama <rodrigogamah4@gmail.com>
2023-11-20 12:25:40 -0300

    estrtura inicial para enviar email

---
+++ src/main/java/com/example/corinthians/domain/mail/MailEstrutura.java
@@ -0,0 +1,11 @@
+package com.example.corinthians.domain.mail;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class MailEstrutura {
+    private String subject;
+    private String message;
+}
---
+++ src/main/java/com/example/corinthians/services/MailService.java
@@ -0,0 +1,17 @@
+package com.example.corinthians.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.mail.MailProperties;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+public class MailService {
+
+    @Autowired
+    private JavaMailSender mailSender;
+
+    @Autowired
+    private MailProperties mailProperties;
+    
+}
